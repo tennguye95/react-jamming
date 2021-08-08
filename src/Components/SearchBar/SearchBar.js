94fec7707f127dac333dcfecb4d4fac5a4e91ef6
sacred95 <sacred9529@gmail.com>
2021-08-08 08:50:59 -0700

    initialized

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event){
+        this.setState({term: event.target.value})
+    }
+    render() {
+        return (
+            <div className="SearchBar">
+                <input onChange = {this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+                <button onClick = {this.search} className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
